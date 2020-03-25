e7f60613336e5be90c2c635fe527200894398325
Fran Acien <amil101ftw@gmail.com>
2020-03-25 11:35:00 +0100

    updating binaries paths

--- schedule_all.sh
+++ schedule_all.sh
@@ -2,19 +2,19 @@
 
 # Update Satellite Information
 
-logger "[WX weather station] Updating TLE from Celestrak"
+/usr/bin/logger "[WX weather station] Updating TLE from Celestrak"
 
-wget -qr https://www.celestrak.com/NORAD/elements/weather.txt -O ${WX_GROUND_DIR}/weather.txt
-grep "NOAA 15" ${WX_GROUND_DIR}/weather.txt -A 2 > ${WX_GROUND_DIR}/weather.tle
-grep "NOAA 18" ${WX_GROUND_DIR}/weather.txt -A 2 >> ${WX_GROUND_DIR}/weather.tle
-grep "NOAA 19" ${WX_GROUND_DIR}/weather.txt -A 2 >> ${WX_GROUND_DIR}/weather.tle
-grep "METEOR-M 2" ${WX_GROUND_DIR}/weather.txt -A 2 >> ${WX_GROUND_DIR}/weather.tle
+/usr/bin/wget -qr https://www.celestrak.com/NORAD/elements/weather.txt -O ${WX_GROUND_DIR}/weather.txt
+/usr/bin/grep "NOAA 15" ${WX_GROUND_DIR}/weather.txt -A 2 > ${WX_GROUND_DIR}/weather.tle
+/usr/bin/grep "NOAA 18" ${WX_GROUND_DIR}/weather.txt -A 2 >> ${WX_GROUND_DIR}/weather.tle
+/usr/bin/grep "NOAA 19" ${WX_GROUND_DIR}/weather.txt -A 2 >> ${WX_GROUND_DIR}/weather.tle
+/usr/bin/grep "METEOR-M 2" ${WX_GROUND_DIR}/weather.txt -A 2 >> ${WX_GROUND_DIR}/weather.tle
 
 #Remove all AT jobs
 
-for i in `atq | awk '{print $1}'`;do atrm $i;done
+for i in `/usr/bin/atq | /usr/bin/awk '{print $1}'`;do /usr/bin/atrm $i;done
 
-rm -f ${WX_GROUND_DIR}/upcoming_passes.txt
+/usr/bin/rm -f ${WX_GROUND_DIR}/upcoming_passes.txt
 
 #Schedule Satellite Passes:
 ${WX_GROUND_DIR}/schedule_satellite.sh "NOAA 19" 137100000 32000 32000 11025
